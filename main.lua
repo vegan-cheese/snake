445e894e6d099360b7788e7a5816b30f9c2cfe60
Programming in F Minor <110762965+programming-in-fm@users.noreply.github.com>
2023-02-11 18:51:04 +0000

    Finished drawing grid squares

--- main.lua
+++ main.lua
@@ -2,8 +2,8 @@ function love.load()
     WINDOW_DIMENSIONS = {}
     WINDOW_DIMENSIONS.width, WINDOW_DIMENSIONS.height = love.window.getMode()
     GRID_AREA_DIMENSIONS = {
-        width = WINDOW_DIMENSIONS.width - 100,
-        height = WINDOW_DIMENSIONS.height - 100
+        width = 500,
+        height = 500
     }
 
     GRID_SIZE = 9
@@ -22,12 +22,43 @@ function love.update()
 end
 
 function love.draw()
-    local grid_square_width = GRID_AREA_DIMENSIONS.width / GRID_SIZE
-    local grid_square_height = GRID_AREA_DIMENSIONS.height / GRID_SIZE
-    for column, column_table in ipairs(GRID) do
-        for row, square_value in ipairs(column_table) do
-            local grid_square_pos_x = grid_square_width * (column - 1) + 50
-            local grid_square_pos_y = grid_square_height * (row - 1) + 50
-        end
-    end
-end
+
+    love.graphics.clear(0, 0.4, 0)
+
+    local grid_square_area = {
+        x_px = GRID_AREA_DIMENSIONS.width / GRID_SIZE,
+        y_px = GRID_AREA_DIMENSIONS.height / GRID_SIZE
+    }
+
+    for column, row in ipairs(GRID) do
+        for index, value in ipairs(row) do
+
+
+            local grid_square_pos = {
+                x_px = grid_square_area.x_px * (column - 1) + 50,
+                y_px = grid_square_area.y_px * (index - 1) + 100
+            }
+
+            -- Draw a rectange for each grid square
+            love.graphics.setColor(0, 0.6, 0)
+            love.graphics.rectangle(
+                "fill",
+                grid_square_pos.x_px,
+                grid_square_pos.y_px,
+                grid_square_area.x_px,
+                grid_square_area.y_px
+            )
+
+            -- Draw an outline for each grid square
+            love.graphics.setColor(0, 0.4, 0)
+            love.graphics.rectangle(
+                "line",
+                grid_square_pos.x_px,
+                grid_square_pos.y_px,
+                grid_square_area.x_px,
+                grid_square_area.y_px
+            )
+
+        end -- row - for loop
+    end -- column - for loop
+end -- love.draw() - function
